5c7e8a8f826e91f423c1a40e48d8bfad7837cdc0
kratix <>
2025-08-03 23:15:31 +0000

    Update from: vpc-test-vpc2-vpcpipeline-0f970.tfe-5058f

---
+++ tfe/tfe/main.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+  source  = "https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v5.19.0"
+
+  name                = "test-vpc2"
+  cidr                = "10.0.0.0/16"
+  enable_nat_gateway  = true
+}
