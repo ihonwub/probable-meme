8dd9fb0f7c6ae7e1226059072add26a87ea32d47
ihonwub <iheanyi.onwubiko@excella.com>
2025-08-03 19:48:47 -0400

    Update main.tf

--- tfe/main.tf
+++ tfe/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.7.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
-
-
 module "vpc" {
   source  = "https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v5.19.0"
 
