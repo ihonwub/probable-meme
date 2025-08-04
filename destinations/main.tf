c0c19919d143c03985bdd6378df88e6cdec60927
kratix <>
2025-08-04 21:05:33 +0000

    Update from: vpc-vpc-vpcpipeline-dca30.tfe-5058f

--- destinations/main.tf
+++ destinations/main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "6.0.1"
 
-  name                = "vpc-example"
+  name                = "vpc"
   cidr                = "10.0.0.0/16"
-  enable_nat_gateway  = true
+  enable_nat_gateway  = false
 }
