a655b47fa20838b162db9fb1c1ef895f95e16907
Iheanyi <ionawsinfra@gmail.com>
2025-08-04 16:32:05 -0400

    try: trigger plan

--- workflows/resource/configure/vpcpipeline/vpc-vpc-pipeline-image/resources/variables.tf
+++ workflows/resource/configure/vpcpipeline/vpc-vpc-pipeline-image/resources/variables.tf
@@ -1,20 +1,20 @@
-variable "name" {
-  type        = string
-  description = "The name of the VPC"
-}
+# variable "name" {
+#   type        = string
+#   description = "The name of the VPC"
+# }
 
-variable "cidr" {
-  type        = string
-  description = "The CIDR block for the VPC"
-}
-variable "enable_nat_gateway" {
-  type        = bool
-  description = "Enable NAT Gateway for the VPC"
-  default     = true
-}
+# variable "cidr" {
+#   type        = string
+#   description = "The CIDR block for the VPC"
+# }
+# variable "enable_nat_gateway" {
+#   type        = bool
+#   description = "Enable NAT Gateway for the VPC"
+#   default     = true
+# }
 
-variable "region" {
-  description = "AWS region to use"
-  type        = string
-  default     = "us-east-1"
-}
+# variable "region" {
+#   description = "AWS region to use"
+#   type        = string
+#   default     = "us-east-1"
+# }
