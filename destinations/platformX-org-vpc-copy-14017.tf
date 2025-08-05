34c02d43c95b11bfd107f70d3c374e9a6e9753b4
kratix <>
2025-08-05 03:30:54 +0000

    Update from: vpc-vpc-copy-vpcpipeline-2a00c.tfe-5058f

---
+++ destinations/platformX-org-vpc-copy-14017.tf
@@ -0,0 +1,16 @@
+module "platformX-org-vpc-copy-14017_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+
+  name                = "vpc-copy"
+  cidr                = "10.0.0.0/16"
+  enable_nat_gateway  = true
+}
+
+output "platformX-org-vpc-copy-14017-vpc_id" {
+  value = module.platformX-org-vpc-copy-14017_vpc.vpc_id
+}
+
+output "platformX-org-vpc-copy-14017-vpc_name" {
+  value = module.platformX-org-vpc-copy-14017_vpc.vpc_name
+}
