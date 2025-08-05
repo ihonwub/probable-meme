6297a861aa2e0d3c947300afd88bb18d1b108796
kratix <>
2025-08-05 03:54:47 +0000

    Update from: vpc-vpc-copy-vpcpipeline-2a00c.tfe-5058f

--- destinations/vpc-copy.tf
+++ destinations/vpc-copy.tf
@@ -4,7 +4,7 @@ module "vpc-copy_vpc" {
 
   name                = "vpc-copy"
   cidr                = "10.0.0.0/16"
-  enable_nat_gateway  = false
+  enable_nat_gateway  = true
 }
 
 output "vpc-copy-vpc_id" {
