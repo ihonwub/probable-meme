0256888b6df651f7bc84ba4966dd4902f6bdda3c
kratix <>
2025-08-07 00:12:43 +0000

    Update from: vpc-vpc-example-configure-18742.tfe-5058f

--- destinations/platformX-org-vpc-example-30149.tf
+++ destinations/platformX-org-vpc-example-30149.tf
@@ -6,11 +6,11 @@ module "platformX-org-vpc-example-30149_vpc" {
   cidr                = "10.0.0.0/16"
   enable_nat_gateway  = false
   create_igw         = false
-  tags                = jsondecode("{
-  "Environment": "dev",
-  "Owner": "DevOps",
-  "Purpose": "test-updates"
-}")
+  tags                = {
+        Environment = "dev"
+    Owner = "DevOps"
+    Purpose = "test-updates"
+  }
 }
 
 output "platformX-org-vpc-example-30149-vpc_id" {
