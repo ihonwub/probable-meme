2d325d9c1aca0983d761f82646b26c824c8492d9
kratix <>
2025-09-06 02:35:23 +0000

    Update from: vpc-vpc-example-configure-1ded0.tf-cloud-5058f

---
+++ destinations/terraform-cloud/platformX-org-vpc-example-2084.tf
@@ -0,0 +1,24 @@
+module "platformX-org-vpc-example-2084_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+
+  name                = "platformX-org-vpc-example-2084"
+  cidr                = "10.0.0.0/16"
+  enable_nat_gateway  = false
+  create_igw         = false
+  tags                = {
+        Environment = "dev"
+    Owner = "@ihonwub"
+    Project = "promise-vpc-hcp"
+    Purpose = "test-updates"
+    Team = "Platform Engineering"
+  }
+}
+
+output "platformX-org-vpc-example-2084-vpc_id" {
+  value = module.platformX-org-vpc-example-2084_vpc.vpc_id
+}
+
+output "platformX-org-vpc-example-2084-vpc_name" {
+  value = module.platformX-org-vpc-example-2084_vpc.name
+}
